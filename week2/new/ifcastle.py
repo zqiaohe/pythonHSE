61a9de735627e047643337dc351749a28d836880
zqiaohe <zanaevab@gmail.com>
2018-11-11 01:38:02 +0300

    castle if with correct signs

--- week2/new/ifcastle.py
+++ week2/new/ifcastle.py
@@ -1,17 +1,17 @@
 a, b, c = int(input()), int(input()), int(input())
 d, e = int(input()), int(input())
-if d * e <= a * b:
-    if (d <= a and e <= b) or (d <= b and e <= a):
+if d * e >= a * b:
+    if (d >= a and e >= b) or (d >= b and e >= a):
         print('YES')
     else:
         print('NO')
-elif d * e <= a * c:
-    if (d <= a and e <= c) or (d <= c and e <= a):
+elif d * e >= a * c:
+    if (d >= a and e >= c) or (d >= c and e >= a):
         print('YES')
     else:
         print('NO')
-elif d * e <= b * c:
-    if (d <= c and e <= b) or (d <= b and e <= ac):
+elif d * e >= b * c:
+    if (d >= c and e >= b) or (d >= b and e >= c):
         print('YES')
     else:
         print('NO')
