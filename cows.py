09416fc13b9266731731292307fae160094bde5a
Занаева Баярма <bayarma.zanaeva@inside.glowbyte.com>
2019-07-23 14:25:05 +0300

    Heap of little chanhes

--- cows.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-if 10 < n < 20:
-    print(str(n) + ' ' + 'korov')
-elif n % 10 == 1:
-    print(str(n) + ' ' + 'korova')
-elif 5 > n % 10 > 1:
-    print(str(n) + ' ' + 'korovy')
-else:
-    print(str(n) + ' ' + 'korov')
--- order.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-if (a <= b <= c):
-    print(a, b, c)
-if (b > a):
-    (a, b) = (b, a)
-elif (c > a):
-    (a, c) = (c, a)
