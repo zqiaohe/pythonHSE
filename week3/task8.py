89aa0d7033ed422e4285891689521fc874588271
zqiaohe <zanaevab@gmail.com>
2019-07-24 16:05:25 +0300

    task 8

---
+++ week3/task8.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+y = (f * a - c * e) / (a * d - c * b)
+x = (e - b * y) / a
+print(x, y)
