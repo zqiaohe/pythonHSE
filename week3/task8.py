e4930a13b97adfd90f19677aaf9fe0d7deab1ec7
zqiaohe <zanaevab@gmail.com>
2019-07-24 16:48:54 +0300

    new task 8

--- week3/task8.py
+++ week3/task8.py
@@ -4,6 +4,13 @@ c = float(input())
 d = float(input())
 e = float(input())
 f = float(input())
-y = (f * a - c * e) / (a * d - c * b)
-x = (e - b * y) / a
+if a != 0 and b != 0 and c != 0 and d != 0:
+    y = (f * a - c * e) / (a * d - c * b)
+    x = (e - b * y) / a
+elif a == 0:
+    y = e / b
+    x = (f - d * y) / c
+elif b == 0:
+    x = e / a
+    y = (f - c * x) / d
 print(x, y)
---
+++ week3/task9.py
@@ -0,0 +1,10 @@
+s = input()
+print(s[2])  # 1
+print(s[-2])  # 2
+print(s[:5])  # 3
+print(s[:-2])  # 4
+print(s[::2])  # 5
+print(s[1::2])  # 6
+print(s[::-1])  # 7
+print(s[-1::-2])  # 8
+print(len(s))  # 9
